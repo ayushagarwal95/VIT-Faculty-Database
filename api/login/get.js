f8ab87c6bcbfe8f9b89821a18d684508ffd8254f
ayushagarwal95 <agarwalayush161@gmail.com>
2015-06-29 23:15:18 +0530

    Fixed Login
    * added dependencies for cache
    * added functionality to use cookies

--- api/login/get.js
+++ api/login/get.js
@@ -1,12 +1,25 @@
 'use strict';
 
 var unirest = require('unirest');
+var cache = require('memory-cache');
 
 exports.get = function (app, data, callback) {
   let captchaUri = 'https://academics.vit.ac.in/student/captcha.asp';
   var onRequest = function (response) {
-      console.log(response.body);
-      callback(null, response.body);
+      if (response.error) {
+          callback(true, null);
+      }
+      else {
+          let validity = 2;
+          let key = Object.keys(response.cookies)[0];
+          let cookieSerial = key + '=' + response.cookies[key];
+          let doc = {
+              reg_no: data.reg_no,
+              cookie: cookieSerial
+          };
+          cache.put(data.reg_no, doc, validity * 60 * 1000);
+          callback(null, response.body);
+      }
     };
   unirest.get(captchaUri)
     .encoding(null)
--- api/login/submit.js
+++ api/login/submit.js
@@ -3,41 +3,55 @@
 var cheerio = require('cheerio');
 var path = require('path');
 var unirest = require('unirest');
+var cache = require('memory-cache');
 
 exports.get = function (app, data, callback) {
-    let submitUri = 'https://academics.vit.ac.in/student/stud_login_submit.asp';
-    console.log(data);
-    var onPost = function (response) {
-        delete data['captcha'];
-        if (response.error) {
-            callback(true, data);
-        }
-        else {
-            try {
-                var scraper = cheerio.load(response.body);
-                var htmlTable = cheerio.load(scraper('table').eq(1).html());
-                let text = htmlTable('td font').eq(0).text();
-                text = text.split(' - ')[0].replace(/[^a-zA-Z0-9]/g, '');
-                if (text === data.reg_no) {
-                    console.log('Login Successful');
-                    callback(null, data)
+    if (cache.get(data.reg_no) !== null) {
+        var CookieJar = unirest.jar();
+        let cookieSerial = cache.get(data.reg_no).cookie;
+        let submitUri = 'https://academics.vit.ac.in/student/stud_login_submit.asp';
+        CookieJar.add(unirest.cookie(cookieSerial), submitUri);
+        var onPost = function (response) {
+            delete data['captcha'];
+            if (response.error) {
+                callback(true, data);
+            }
+            else {
+                try {
+                    var scraper = cheerio.load(response.body);
+                    var htmlTable = cheerio.load(scraper('table').eq(1).html());
+                    let text = htmlTable('td font').eq(0).text();
+                    text = text.split(' - ')[0].replace(/[^a-zA-Z0-9]/g, '');
+                    if (text === data.reg_no) {
+                        let validity = 3;
+                        let doc = {
+                            reg_no: data.reg_no,
+                            password: data.password,
+                            cookie: cookieSerial
+                        };
+                        cache.put(data.reg_no, doc, validity * 60 * 1000);
+                        console.log('Login Successful');
+                        callback(null, data)
+                    }
+                    else {
+                        console.log('Error in Login or Invalid Credentials');
+                        callback(true, data);
+                    }
                 }
-                else {
-                    console.log('Error in Login');
+                catch (ex) {
+                    console.log('Error in Scraping');
                     callback(true, data);
                 }
             }
-            catch (ex) {
-                console.log('Error in Scraping');
-                callback(true, data);
-            }
-        }
-    };
-    unirest.post(submitUri)
-        .form({
-            regno: data.reg_no,
-            passwd: data.password,
-            vrfcd: data.captcha
-        })
-        .end(onPost);
+        };
+        unirest.post(submitUri)
+            .jar(CookieJar)
+            .form({
+                regno: data.reg_no,
+                passwd: data.password,
+                vrfcd: data.captcha
+            })
+            .timeout(28000)
+            .end(onPost);
+    }
 };
