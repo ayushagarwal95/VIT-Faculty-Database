808948b458fee7be9ffa5e5748950a5d13951dfa
ayushagarwal95 <agarwalayush161@gmail.com>
2015-06-30 13:56:17 +0530

    Structured Code
    * Using cookies for requesting the faculty information page

--- api/scraper/aggregate.js
+++ api/scraper/aggregate.js
@@ -6,37 +6,47 @@ var cache = require('memory-cache');
 
 exports.get = function (app, data, callback) {
     let facultyInfoUri = 'https://academics.vit.ac.in/student/getfacdet.asp';
-    let date = new Date();
+    let date = new Date().toUTCString();
     var CookieJar = unirest.jar();
     let cookieSerial = cache.get(data.reg_no).cookie;
-    let onGet = function (response){
+    let onGet = function (response) {
         if (response.error) {
             callback(true, null);
         }
         else {
-            try {
+            if (cache.get(data.reg_no) !== null) {
                 console.log(response.body);
-                var scraper = cheerio.load(response.body);
-                scraper = cheerio.load(scraper('table table').eq(1).html());
-                var forEach = function (i, element) {
-                    let htmlRow = cheerio.load(scraper(this).html());
-                    let htmlColumn = htmlRow('td');
-                    if (i > 0) {
-                        let link = htmlColumn.eq(3).attr('href');
-                        console.log(link);
+                let cacheDoc = cache.get(data.reg_no);
+                if (cacheDoc.password === data.password) {
+                    try {
+                        var scraper = cheerio.load(response.body);
+                        scraper = cheerio.load(scraper('table table').eq(1).html());
+                        var forEach = function (i, element) {
+                            let htmlRow = cheerio.load(scraper(this).html());
+                            let htmlColumn = htmlRow('td');
+                            if (i > 0) {
+                                let link = htmlColumn.eq(3).attr('href');
+                                console.log(link);
+                            }
+                        };
+                        scraper('tr').each(forEach);
+                        callback(null, data);
                     }
-                };
-                scraper('tr').each(forEach);
-            }
-            catch (ex) {
-                callback(true, null);
+                    catch (ex) {
+                        callback(true, null);
+                    }
+                }
+                else {
+                    console.log('Invalid Credentials');
+                    callback(true, null);
+                }
             }
         }
     };
     CookieJar.add(unirest.cookie(cookieSerial), facultyInfoUri);
     unirest.get(facultyInfoUri)
         .query({'fac': null})
-        .query({'x': date.toUTCString()})
+        .query({'x': date})
         .jar(CookieJar)
         .end(onGet);
 };
